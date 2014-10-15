08ef9bfaff5cd5a9d53b08b779fd7444bf6c7a7e
Mouli <chandramouli.balasubramanian@gmail.com>
2014-10-15 15:58:59 -0700

    closures - scope in javascript...

---
+++ example.js
@@ -0,0 +1,15 @@
+#!/usr/local/bin/node
+
+function example() {
+  var o = {}, i = 0;
+  for (i = 0; i < 3; i++) {
+    o[i] = function() { console.log(i); };
+  }
+  i = 10;
+  
+  o[0]();
+  o[1]();
+  o[2]();
+}
+
+example();
