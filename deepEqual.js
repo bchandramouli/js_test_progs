4960d805245d4d850953daa85ddc87dfebffd040
Mouli <chandramouli.balasubramanian@gmail.com>
2014-10-24 14:15:57 -0700

    Some more test cases and boundary checks.

--- deepEqual.js
+++ deepEqual.js
@@ -1,18 +1,25 @@
 
+
 function deepEqual(obj1, obj2) {
 	if (typeof(obj1) !== typeof(obj2)) {
 		return (false);
 	}
+
+	if ((obj1 !== null) && (obj2 === null)) {
+		return (false);
+	}
+
+	if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+		return (false);
+    }
 	
 	for (var key in obj1) {
 		//console.log(key);
-		if ((obj2 === null) || 
-			//(obj1.key !== obj2.key) ||
-		    (!obj2.hasOwnProperty(key))) {
+		if (!obj2.hasOwnProperty(key)) {
 			return (false);
 		}
 		
-		if ((obj1[key] !== null) && (typeof obj1[key] === "object")) {
+		if ((obj1[key] != null) && (typeof obj1[key] === "object")) {
 			return (deepEqual(obj1[key], obj2[key]));
 		}
 	}
@@ -29,4 +36,7 @@ console.log(deepEqual(obj, {here: 1, object: 2}));
 // → false
 console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
 // → true
-
+console.log(deepEqual(obj, {here: {is: "an", blah: "bubllikando"}, object: 2}));
+// → false
+console.log(deepEqual(null, null));
+// → false
